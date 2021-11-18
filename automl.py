b704bd1ea3a6b33d34a96b2fdd4f8f19bc12368c
MarkEnglander <markpenglander@gmail.com>
2021-11-18 00:04:16 +0000

    actual_progress
    have basic tests running on sklearn classifiers

--- automl.py
+++ automl.py
@@ -1,18 +1,30 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier
 from sklearn.datasets import make_classification
+from sklearn.ensemble import AdaBoostClassifier
 import pandas as pd
 import numpy as np
 
 
 THRESHOLD = 0.001
-DATA_LEN = 20
+TRAIN_DATA_LEN = 40000
+TEST_DATA_LEN = 10000
+BIGGEST = 2 ** 50
 
 
 def clean_dataset(df):
     assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
     df.dropna(inplace=True)
-    indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
-    return df[indices_to_keep].astype(np.float64)
+    # indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
+    df.replace([np.inf, -np.inf, np.nan], 0, inplace=True)
+    df = df.astype(float)
+    # df = df[indices_to_keep].astype(np.float64)
+    df = df.reset_index()
+    # df = np.nan_to_num(df)
+    # print(df.max(axis=1))
+    # df.drop( > BIGGEST].index, inplace=True)
+    df = df.loc[:, (df.max() < BIGGEST)]
+    return df
 
 
 def separate_into_xy(dff):
@@ -22,33 +34,63 @@ def separate_into_xy(dff):
     return X, y
 
 
-def split(dff, split=None):
+def split(dff, split=None, split2=None):
     if split is None:
         split = int(len(dff) / 2)
-    print(split)
+    if split2 is None:
+        split2 = int(len(dff) / 2)
     df1 = dff.iloc[:split, :]
-    df2 = dff.iloc[split + 1:, :]
+    df2 = dff.iloc[split2 + 1:, :]
     return df1, df2
+
+
+def accuracy(y, test_y):
+    y_arr, test_y_arr = np.array(y), np.array(test_y)
+    total = len(y_arr)
+    correct = 0
+    for i in range(len(y_arr)):
+        if y_arr[i] == test_y_arr[i]:
+            correct += 1
+    return correct / total
+
+
+def recall(y, test_y):
+    y_arr, test_y_arr = np.array(y), np.array(test_y)
+    total = 0
+    recalled = 0
+    for i in range(len(y_arr)):
+        if y_arr[i] == 1:
+            total += 1
+            if test_y_arr[i] == 1:
+                recalled += 1
+    if total == 0 or recalled == 0:
+        if 1 not in y_arr or 1 not in test_y_arr:
+            raise Exception('This model predicted 0 for everything')
+    return recalled / total
 
 
 df1 = pd.read_csv('data/train_v2.csv')
 df1 = clean_dataset(df1)
+print("The len of the dataset after cleaning is: " + str(len(df1)))
 # testing_df = pd.read_csv('data/test_v2.csv')
-df, testing_df = split(df1)
-
-df = df.head(DATA_LEN)
-testing_df = testing_df.head(DATA_LEN)
+df, testing_df = split(df1, TRAIN_DATA_LEN, TEST_DATA_LEN)
 
 X, y = separate_into_xy(df)
 test_X, test_y = separate_into_xy(testing_df)
 
-X = X.reset_index()
-y = y.reset_index()
+# clf = RandomForestClassifier(max_depth=5, random_state=0)
+# clf = BaggingClassifier(random_state=0)
+clf = AdaBoostClassifier(n_estimators=100)
 
-clf = RandomForestClassifier(max_depth=2, random_state=0)
 clf.fit(X, y)
+z = clf.predict(test_X)
 
-print("something worked")
+print("predicted labels: " + str(z))
+print("real labels: " + str(np.array(test_y)))
+print("accuracy: " + str(accuracy(test_y, z)))
+print("recall: " + str(recall(test_y, z)))
+
+print("if you got here then something might have worked")
 
 
 
